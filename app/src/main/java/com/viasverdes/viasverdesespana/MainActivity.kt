07392d0821e0381c3450741831d57e71fd9793cb
José Fuentes <ydrojen@gmail.com>
2018-07-24 21:14:37 +0200

    Close #3 Añadir la Bottom Navigation View

--- app/src/main/java/com/viasverdes/viasverdesespana/MainActivity.kt
+++ app/src/main/java/com/viasverdes/viasverdesespana/MainActivity.kt
@@ -2,11 +2,18 @@ package com.viasverdes.viasverdesespana
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import butterknife.BindView
+import butterknife.ButterKnife
+import com.viasverdes.viasverdesespana.utils.removeShiftMode
 
 class MainActivity : AppCompatActivity() {
+    @BindView(R.id.main__bottom_navigation) lateinit var bottomNavigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ButterKnife.bind(this)
+        bottomNavigationView.removeShiftMode()
     }
 }
---
+++ app/src/main/java/com/viasverdes/viasverdesespana/utils/BottomNavigationViewHelper.kt
@@ -0,0 +1,28 @@
+package com.viasverdes.viasverdesespana.utils
+
+import android.annotation.SuppressLint
+import android.support.design.internal.BottomNavigationItemView
+import android.support.design.internal.BottomNavigationMenuView
+import android.support.design.widget.BottomNavigationView
+import android.util.Log
+
+@SuppressLint("RestrictedApi")
+fun BottomNavigationView.removeShiftMode() {
+    val menuView = this.getChildAt(0) as BottomNavigationMenuView
+    try {
+        val shiftingMode = menuView.javaClass.getDeclaredField("mShiftingMode")
+        shiftingMode.isAccessible = true
+        shiftingMode.setBoolean(menuView, false)
+        shiftingMode.isAccessible = false
+        for (i in 0 until menuView.childCount) {
+            val item = menuView.getChildAt(i) as BottomNavigationItemView
+            item.setShiftingMode(false)
+            // set once again checked value, so view will be updated
+            item.setChecked(item.itemData.isChecked)
+        }
+    } catch (e: NoSuchFieldException) {
+        Log.e("ERROR NO SUCH FIELD", "Unable to get shift mode field")
+    } catch (e: IllegalAccessException) {
+        Log.e("ERROR ILLEGAL ALG", "Unable to change value of shift mode")
+    }
+}
