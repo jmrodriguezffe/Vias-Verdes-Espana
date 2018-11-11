ee5f1985de8348fbab55e655ff091cd508972e7d
José Fuentes <ydrojen@gmail.com>
2018-11-11 16:40:20 +0100

    +INFO: Terminar sección Acerca de...

--- app/src/main/java/com/viasverdes/viasverdesespana/ui/fragment/InfoAboutFragment.kt
+++ app/src/main/java/com/viasverdes/viasverdesespana/ui/fragment/InfoAboutFragment.kt
@@ -3,6 +3,7 @@ package com.viasverdes.viasverdesespana.ui.fragment
 import android.os.Bundle
 import com.underlegendz.corelegendz.vm.VMFragment
 import com.viasverdes.viasverdesespana.R
+import kotlinx.android.synthetic.main.fragment__more_info__about.*
 
 
 class InfoAboutFragment : VMFragment() {
@@ -20,6 +21,7 @@ class InfoAboutFragment : VMFragment() {
 
 
   override fun initializeView() {
+    more_info__about__webview.loadUrl("file:///android_asset/about.html")
   }
 
   override fun getLayoutResource(): Int {
